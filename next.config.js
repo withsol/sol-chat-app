9aa3cb8c791d004afc80f58b0675fd33e95dca71
withsol <kelsey@thealignedbusiness.com>
2025-09-08 17:06:02 +0000

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
